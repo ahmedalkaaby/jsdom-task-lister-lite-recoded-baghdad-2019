5e947c94813d6eb25ad5e595048eefedd970b93a
K3be <ahmed.13.alkaaby@gmail.com>
2019-09-29 15:54:56 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
 
-let input= document.getElementById()
+let input= document.getElementById("new-task-description");
 function todo(){
+  document.preventDefault();
   let inputval= input.value;
+  
   
 }
