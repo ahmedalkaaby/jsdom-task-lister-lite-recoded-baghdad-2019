acf19af4517ac3bfc013f592fec58ed6bc45eb81
K3be <ahmed.13.alkaaby@gmail.com>
2019-09-29 15:57:37 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 let input= document.getElementById("new-task-description");
+let list=document.getElementById("tasks");
+
 function todo(event){
   event.preventDefault();
   let inputval= input.value;
-  
+  list=document.createElemen('li');
+  list
   
   
 }
